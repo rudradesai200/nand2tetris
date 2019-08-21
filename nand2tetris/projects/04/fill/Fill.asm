3373e6d8fc4aaeb8b17a6e74bfda5aca3a399d04
Rudra Desai <44108388+rudradesai200@users.noreply.github.com>
2019-08-21 09:53:20 +0530

    Add files via upload
    changes in fill.asm

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -4,68 +4,52 @@
 // File name: projects/04/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// When a key is pressed (any key), the program SCREENOFFens the screen,
+// i.e. writes "SCREENOFF" in every pixel;
+// the screen should remain fully SCREENOFF as long as the key is pressed. 
 // When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
+// "SCREENON" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 // screen = screen + r.32 + c/16;
 // Put your code here.
 
-@SCREEN
+@8192 
 D=A
-@R0
-M=D
-@8191
-D=D+A
-@R2
-M=D
+@R1
+M=D   
 
 (LOOP)
-@SCREEN
-D=A
 @R0
-M=D
-D=0
-@KBD
-D=D-M
+M=0   
 
-@SCREENON
-D;JGT
+(INNER)
+@KBD
+D=M
 @SCREENOFF
-0;JMP
+D;JEQ   
 
 (SCREENON)
 @R0
-A=M
-D=A
-M=1
-@R2
-D=D-M
-@LOOP
-D;JGE
-@R0
-M=M+1
-@SCREENON
-0;JMP
-
+D=M
+@SCREEN
+A=A+D   
+M=-1    
+@END
+0;JMP   
 
 (SCREENOFF)
 @R0
-A=M
-D=A
-M=0
-@R2
+D=M
+@SCREEN
+A=A+D  
+M=0    
+
+(END)   
+@R0
+MD=M+1 
+@R1
 D=D-M
 @LOOP
-D;JGE
-@R0
-M=M+1
-@SCREENOFF
-0;JMP
-
-
-
-
-
+D;JEQ  
+@INNER
+0;JMP  
